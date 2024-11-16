50e4d22b6e7e918442903e3f7b21ebf7aea3d15f
merlos <merlos@users.noreply.github.com>
2024-11-16 05:05:25 +0300

    add constrains and uniqueness in id

--- db/migrate/20241116015841_create_messages.rb
+++ db/migrate/20241116015841_create_messages.rb
@@ -1,12 +1,12 @@
 class CreateMessages < ActiveRecord::Migration[8.0]
   def change
-    create_table :messages do |t|
-      t.text :encrypted_content
-      t.integer :views_remaining
+    create_table :messages, id: false do |t|
+      t.string :id, null: false, primary_key: true
+      t.text :encrypted_content, null: false
+      t.integer :views_remaining, null: false, default: 1
       t.datetime :expiration_time
-      t.string :id
-
       t.timestamps
     end
+    add_index :messages, :id, unique: true
   end
 end
