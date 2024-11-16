e906faa8a2c01a6393f7b0427eb6e5abac63e221
merlos <merlos@users.noreply.github.com>
2024-11-16 05:09:01 +0300

    add bcrypt and encription helper

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,14 @@
 class Message < ApplicationRecord
-end
+    include EncryptionHelper
+  
+    before_create :set_defaults
+  
+    def set_defaults
+      self.id ||= generate_random_string(10)
+    end
+  
+    def self.auto_delete_expired
+      where("views_remaining <= 0 OR expiration_time <= ?", Time.current).destroy_all
+    end
+  end
+  
