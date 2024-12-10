851f51c91a1916973f66a8722bcf3eb41649c234
merlos <merlos@users.noreply.github.com>
2024-12-10 19:02:55 +0300

    add the job logic to expire messages

--- app/jobs/expire_message_job.rb
+++ app/jobs/expire_message_job.rb
@@ -1,7 +1,8 @@
 class ExpireMessageJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(message_id)
+    message = Message.find_by(id: message_id)
+    message.auto_delete_expired if message
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ApplicationRecord
     include EncryptionHelper
 
+    after_create :schedule_expiration_job
+
     MAX_ALLOWED_TIME = 1.year + 1.day
 
     validates :views_remaining, numericality: { only_integer: true, greater_than: 0, less_than: 51 }
@@ -16,8 +18,19 @@ class Message < ApplicationRecord
       where("views_remaining <= 0 OR expiration_time <= ?", Time.current).destroy_all
     end
 
+    def auto_delete_expired
+      destroy if has_expired
+    end
+
+    def has_expired
+      Time.current >= expiration_time
+    end
 
     private
+
+    def schedule_expiration_job
+      ExpireMessageJob.set(wait_until: expiration_time).perform_later(id)
+    end
 
     def expiration_time_within_limit
       if expiration_time.present? && expiration_time > Time.current + MAX_ALLOWED_TIME
